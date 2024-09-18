38d37c4632624a9d5093d3bd98b7c8dc9e4796e9
INJOON CHOI <dlswns8679@naver.com>
2024-09-18 16:20:03 +0900

    first commit

---
+++ src/test/java/com/example/study/QuerydslApplicationTests.java
@@ -0,0 +1,35 @@
+package com.example.study;
+
+import com.example.study.entity.Hello;
+import com.example.study.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		Hello hello  = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = new QHello("h");
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(hello);
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+
+}
---
+++ src/test/java/com/example/study/QuerydslBasicTest.java
@@ -0,0 +1,124 @@
+package com.example.study;
+
+
+import com.example.study.entity.Member;
+import com.example.study.entity.QMember;
+import com.example.study.entity.Team;
+import com.querydsl.core.QueryResults;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.example.study.entity.QMember.member;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    @BeforeEach
+    public void before() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL() {
+        String qlString = "select m from Member m " + "where m.username = :username";
+        Member findMember = em.createQuery(qlString, Member.class)
+                        .setParameter("username", "member1")
+                                .getSingleResult();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+
+    @Test
+    public void startQuerydsl() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QMember m = new QMember("m");
+
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+
+    @Test
+    public void startQuerydsl2() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+
+    @Test
+    public void search() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1").and(member.age.eq(10)))
+                .fetchOne();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+
+    @Test
+    public void searchAndParam() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        List<Member> result1 = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"), member.age.eq(10)).fetch();
+        Assertions.assertThat(result1.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void resultFetch() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        List<Member> fetch = queryFactory.selectFrom(member).fetch();
+
+        //단 건
+        Member findMember1 = queryFactory.selectFrom(member).fetchOne();
+
+        //처음 한 건 조회
+        Member findMember2 = queryFactory.selectFrom(member).fetchFirst();
+
+        //페이징에서 사용
+        QueryResults<Member> results = queryFactory.selectFrom(member).fetchResults();
+
+        //count 쿼리로 변경
+        long count = queryFactory.selectFrom(member).fetchCount();
+
+    }
+}
